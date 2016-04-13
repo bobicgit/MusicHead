7c87e61385ec8ca94046ea55bf2f2345b0c24a2c
bobicgit <bobicgit@gmail.com>
2016-04-13 15:44:43 +0200

    Change iframes for thumbnails. One iframe to load clips into it. Extending cache options in factory. Filtring display via artists

--- src/app/components/YouTube/youtube.factory.js
+++ src/app/components/YouTube/youtube.factory.js
@@ -3,18 +3,24 @@
 	'use strict';
 
 	angular
-	.module('musicHead')
-	.factory('youtube', youtube);
+		.module('musicHead')
+		.factory('youtube', youtube);
 
 	youtube.$inject = ['$http','youtubeDataService'];
 
 	function youtube($http, youtubeDataService) {
 
 	// Accessible members
-
+		var cache = [];
+		var cacheInput = [];
+		var cacheUrlId = '';
 		var factory = {
-			initConnection: initConnection,
-			showItems: showItems
+			showItems: showItems,
+			readCache: readCache,
+			cacheArray: cacheArray,
+			readInputFromCache: readInputFromCache,
+			cacheUrl: cacheUrl,
+			readCacheUrlId: readCacheUrlId
 		};
 
 	// Returning object of functions
@@ -23,26 +29,37 @@
 
 	// Functions declatarions
 
-		function initConnection() { 
-
-			return $http({
-				method: 'GET',
-				url: 'https://content.googleapis.com/youtube/v3/search?',
-				params: {
-						q:'madonna',
-						part:'snippet',
-						key: 'AIzaSyC9Ye6LYcxQHxyMRECr_kFNoCJ13QJHinA'
-					}
-				});
-		};
-
 		function showItems(query) {
 			var ytRequestObject = youtubeDataService.getDataObject("search", query);
 			return $http({
                 method: 'GET',
                 url: ytRequestObject.url,
                 params: ytRequestObject.params
-            });
+            })
+            .then(function(response) {
+            	cache = cache.concat(response.data.items);
+            	return response.data.items;
+            })
 		};
+
+		function readCache() {
+			return cache;
+		};
+
+		function cacheArray(arr) {
+			cacheInput = arr;
+		};
+
+		function readInputFromCache() {
+			return cacheInput;
+		};
+
+		function cacheUrl(id) {
+			cacheUrlId = id;
+		};
+
+		function readCacheUrlId() {
+			return cacheUrlId;
+		}
 	}
 })();
--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -10,18 +10,21 @@
     var vm = this;
 
     vm.addArtists = addArtists;
-    vm.artistsList = 'Madonna, El Guincho, Placebo';
-    vm.artistsArray = [];
+    vm.artistsList;
+    var artistsArray = [];
+    vm.artistsArrayTrimmed;
     vm.classAnimation = '';
     vm.clips = [];
     vm.creationDate = 1460370332351;
     vm.routeArtist = $routeParams.artist;
     vm.showToastr = showToastr;
     vm.splitArtists = splitArtists;
-    vm.redirect = redirect;
-    vm.getTrustedIframeSrc = getTrustedIframeSrc;
-    vm.showRouteArtist = showRouteArtist;
-
+    vm.generateArtists = generateArtists;
+    vm.getTrustedThumbnailSrc = getTrustedThumbnailSrc;
+    vm.getFilter = getFilter();
+    vm.changeVideo = changeVideo;
+    vm.videoUrl = '';
+    vm.loadDefVideo = loadDefVideo;
     // activate();
 
     // function activate() {
@@ -33,30 +36,101 @@
     //         });
     // }
 
+    vm.clips = youtube.readCache();
+    vm.artistsArrayTrimmed = youtube.readInputFromCache();
+    vm.videoUrl = getTrustedIframeSrc(youtube.readCacheUrlId());
 
-    function getTrustedIframeSrc(id) {
-      return trustLink(getIframeSrc(id));
-    }
-    function splitArtists() {
-      vm.artistsArray = vm.artistsList.split(",");
-      console.log(vm.artistsArray);
-      for(var i = 0 ; i<vm.artistsArray.length; i++) {
-        vm.addArtists(vm.artistsArray[i]);
+    function getFilter(){
+      if(vm.routeArtist === 'undefined') {
+        vm.routeArtist = '';
       }
+      console.log('');
+      return {snippet: {title: vm.routeArtist}};
     }
+
+    function loadDefVideo(artist) {
+      var myClips = youtube.readCache();
+      console.log(artist, myClips);
+      for(var i = 0 ; i < myClips.length ; i++) {debugger;
+        
+        console.log(myClips[i].snippet.title.indexOf(artist))
+//        var jedna = if(myClips[i].snippet.title.tolowercase
+          //var druga = (artist.tolowercase) > -1) {
+        if(myClips[i].snippet.title.indexOf(artist) > -1) {
+
+
+
+        if(myClips[i].snippet.title.indexOf(artist) > -1) {
+            youtube.cacheUrl(myClips[i].id.videoId);
+            break;
+          }
+        }
+    };
+
+    function splitArtists() {
+      artistsArray = vm.artistsList.split(",");
+      vm.artistsArrayTrimmed = trimmingArray(artistsArray); 
+      youtube.cacheArray(vm.artistsArrayTrimmed);  
+    }
+
+    function generateArtists() { 
+      splitArtists(); 
+      for(var i = 0 ; i<vm.artistsArrayTrimmed.length; i++) {
+        vm.addArtists(vm.artistsArrayTrimmed[i]);
+      }
+    };
 
     function addArtists(artist) {
       vm.clips = [];
       youtube
       .showItems(artist)
-      .then(function(response) {
+      .then(function(items) {
         vm.artistsList = '';
-        angular.forEach(response.data.items, function(item, index) {
+        vm.videoUrl = getTrustedIframeSrc(items[0].id.videoId);
+        angular.forEach(items, function(item, index) {
           vm.clips.push(item);
-          shuffle(vm.clips);
-          
+          // console.log(item);
+          shuffle(vm.clips);    
         });
       })
+    };
+
+    function changeVideo(video) {
+      vm.videoUrl = getTrustedIframeSrc(video.id.videoId);
+      console.log(video);
+    }
+
+///////////// ESCAPING UNTRUSTED LINKS
+
+    function getTrustedThumbnailSrc(id) {
+      return trustLink(getThumbnailSrc(id));
+    }
+
+    function getThumbnailSrc(videoId) {
+      return 'http://img.youtube.com/vi/' + videoId + '/default.jpg';
+    };
+
+    function getTrustedIframeSrc(id) {
+      return trustLink(getIframeSrc(id));
+    }
+
+    function getIframeSrc(videoId) {
+      return 'http://www.youtube.com/embed/' + videoId + '?autoplay=1';   
+    };
+
+    function trustLink(src) {
+      return $sce.trustAsResourceUrl(src);
+    };
+
+////////// HELPERS
+
+    function trimmingArray(arr) {
+      var trimmedArray = [];
+      angular.forEach(arr, function(item) {
+        item = item.trim();
+        trimmedArray.push(item);
+      });   
+      return trimmedArray;   
     }
 
     function shuffle(array) {
@@ -73,38 +147,12 @@
         array[randomIndex] = temporaryValue;
       }
       return array;
-  }
-
-    function getIframeSrc(videoId) {
-      return 'https://www.youtube.com/embed/' + videoId;
-    };
-
-    function trustLink(src) {
-      return $sce.trustAsResourceUrl(src);
-    };
+    }
 
     function showToastr() {
       toastr.info('Fork <a href="https://github.com/Swiip/generator-gulp-angular" target="_blank"><b>generator-gulp-angular</b></a>');
       vm.classAnimation = '';
     };
-
-    function redirect(artist) {
-      artist = artist.trim();
-      console.log(artist);
-      window.location = ("#/" + artist);
-      showRouteArtist(artist);
-    };
-
-    function showRouteArtist(artist) {
-      console.log(vm.clips);
-      vm.clips = vm.clips.filter(function(clip) { 
-        if (clip.snippet.title.indexOf(artist) > -1) {debugger;
-          return clip;
-        }
-      });
-    //       var string = "foo",
-    //     substring = "oo";
-    // console.log(string.indexOf(substring) > -1);
-    }
   }
+
 })();
